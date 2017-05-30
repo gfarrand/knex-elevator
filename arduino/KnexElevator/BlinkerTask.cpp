f542fa67c349dec814b255b64f2bc95148ae9f80
Grant Farrand <grant.farrand@gmail.com>
2017-05-29 17:05:46 -0700

    Updated LICENSE; blinker task now maps a button state to the debug LED

--- arduino/KnexElevator/BlinkerTask.cpp
+++ arduino/KnexElevator/BlinkerTask.cpp
@@ -1,5 +1,27 @@
 // /////////////////////////////////////////////////////////////////////////////
-// COPYRIGHT blah blah...
+//
+// MIT License
+//
+// Copyright (c) 2017 gfarrand (grant.farrand@gmail.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
 // /////////////////////////////////////////////////////////////////////////////
 
 
@@ -19,12 +41,14 @@
 ////////////////////////////////////////////////////////////////////////////////
 BlinkerTask::BlinkerTask(void)
 {
+    // Intentionally left empty
 }
 
 
 ////////////////////////////////////////////////////////////////////////////////
 BlinkerTask::~BlinkerTask(void)
 {
+    // Intentionally left empty
 }
 
 
@@ -34,8 +58,9 @@ BlinkerTask::initialize(void)
 {
     // Setup the debug LED for writing
     pinMode(13, OUTPUT);
+    pinMode(2, INPUT_PULLUP);
 
-    // Get our task going
+    // Fire up our task
     xTaskCreate(BlinkerTask::runWrapper,  // Entry Point
                 "Main",                   // Task Name
                 128,                      // Stack Depth
@@ -50,6 +75,7 @@ BlinkerTask::initialize(void)
 void
 BlinkerTask::runWrapper(void * pvParameters)
 {
+    // Entry point to the new task
     BlinkerTask* p_this = reinterpret_cast<BlinkerTask*>(pvParameters);
     p_this->run();
 }
@@ -60,10 +86,13 @@ BlinkerTask::run(void)
 {
     while (true)
     {
-        digitalWrite(13, LOW);
-        delay(500);
-        digitalWrite(13, HIGH);
-        delay(500);
+        // Light the debug LED when the button is pressed
+        int buttonState = digitalRead(2);
+        int ledState = (LOW == buttonState) ? HIGH : LOW;
+        digitalWrite(13, ledState);
+
+        // And check back again in 5 milliseconds
+        vTaskDelay(pdMS_TO_TICKS(5));
     }
 }
 
